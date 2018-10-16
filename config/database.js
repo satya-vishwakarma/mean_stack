a1ceb53d211c0f9a96311234a44edf7c20a4cd5c
satya9125@gmail.com <satya9125@gmail.com>
2018-10-16 11:37:41 +0530

    # env setup

--- config/database.js
+++ config/database.js
@@ -1,6 +1,7 @@
+require('dotenv').config();
 module.exports = {
   // configure the code below with your username, password and mlab database information
  // database: 'mongodb://<username>:<password>@ds12226.mlab.com:12226/meanauthapp',   //prod
-  database: 'mongodb://localhost:27017/meanauth',    //dev
+  database: process.env.DATABASE_URL,    //dev
   secret: 'yoursecret'
 }
