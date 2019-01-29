ec62f3fcd17d376eac2d484dbb5c2bd7b4b9ee97
satya <satya9125@gmail.com>
2019-01-29 06:48:59 +0530

    session variable

--- routes/users.js
+++ routes/users.js
@@ -4,11 +4,10 @@ const passport = require('passport');
 const jwt = require('jsonwebtoken');
 const config = require('../config/database');
 const User = require('../models/user');
-const common = require ('../common/common');
-
+const common = require('../common/common');
 // Register
 router.post('/register', (req, res, next) => {
-  let newUser = new User ({
+  let newUser = new User({
     name: req.body.name,
     email: req.body.email,
     username: req.body.username,
@@ -16,10 +15,10 @@ router.post('/register', (req, res, next) => {
   });
 
   User.addUser(newUser, (err, user) => {
-    if(err) {
-      res.json({success: false, msg: 'Failed to register user'});
+    if (err) {
+      res.json({ success: false, msg: 'Failed to register user' });
     } else {
-      res.status(201).json({success: true, msg: 'New User register successfully...'});
+      res.status(201).json({ success: true, msg: 'New User register successfully...' });
     }
   });
 });
@@ -29,20 +28,20 @@ router.post('/authenticate', (req, res, next) => {
   const username = req.body.username;
   const password = req.body.password;
   User.getUserByUsername(username, (err, user) => {
-    if(err) throw err;
-    if(!user) {
-      return res.status(401).json({"status":401 ,"messages":"Authenticattion Required"});      
+    if (err) throw err;
+    if (!user) {
+      return res.status(401).json({ "status": 401, "messages": "Authenticattion Required" });
     }
 
     User.comparePassword(password, user.password, (err, isMatch) => {
-      if(err) throw err;
-      if(isMatch) {
-        const token = jwt.sign({data: user}, config.secret, {
+      if (err) throw err;
+      if (isMatch) {
+        const token = jwt.sign({ data: user }, config.secret, {
           expiresIn: 604800 // 1 week
         });
         res.json({
           success: true,
-          token: 'JWT '+token,
+          token: 'JWT ' + token,
           user: {
             id: user._id,
             name: user.name,
@@ -51,26 +50,43 @@ router.post('/authenticate', (req, res, next) => {
           }
         })
       } else {
-        return res.status(401).json({"status":401 ,"messages":" The username or password is incorrect"});  
+        return res.status(401).json({ "status": 401, "messages": " The username or password is incorrect" });
       }
     });
   });
 });
 
 // Profile
-router.get('/profile', passport.authenticate('jwt', {session:false}), (req, res, next) => {
-  res.json({user: req.user});
+router.get('/profile', passport.authenticate('jwt', { session: false }), (req, res, next) => {
+  res.json({ user: req.user });
 });
 
 
 /**
  * user list 
  */
-router.get('/users',passport.authenticate('jwt', {session:false}), (req, res)=>{
-  User.users(function (err,users){
-    if(err) throw err;    
-    return res.status(200).json(common.response(200,null ,users.length, users));
-   });
+router.get('/users', passport.authenticate('jwt', { session: false }), (req, res) => {
+  User.users(function (err, users) {
+    if (err) throw err;
+    return res.status(200).json(common.response(200, null, users.length, users));
+  });
+});
+
+/**
+ * Set session variable
+ */
+router.post('/set-session', (req, res) => {
+  req.session.email = req.body.email;
+  if(typeof req.session.email ==  'undefined')
+    return res.status(422).json('Please send "email" in post data');
+  return res.status(200).json(`${req.session.email} set session data`);
+});
+
+/**
+ * Get session variable data
+ */
+router.get('/get-session', (req, res) => {
+  return res.status(200).json(`${req.session.email} get session data`);
 });
 
 module.exports = router;
